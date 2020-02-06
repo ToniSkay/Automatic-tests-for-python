e8ab40382f3c72d2a51b31167d15336d0a1e554e
ToniSkay <59788580+ToniSkay@users.noreply.github.com>
2020-02-06 20:11:09 +0300

    Добавили проверку регистрации
    и проверку на то что авторизованный пользователь может добавить товар в корзину

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,9 @@
 from .base_page import BasePage
 from .product_page import PageObject
+from .locator import AddToBasketTest
+from .login_page import LoginPage
 import pytest
 
-''''@pytest.mark.xfail
-@pytest.mark.parametrize('link', ["?promo=offer0", "?promo=offer1", "?promo=offer2", "?promo=offer3", "?promo=offer4",
-                                  "?promo=offer5", "?promo=offer6", "?promo=offer7", "?promo=offer8", "?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
     link = f'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/{link}'
     page = BasePage(browser, link)
@@ -29,16 +28,41 @@ def test_negative_checks1(browser):
 def test_negative_checks2(browser):
     link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207'
     page_object = PageObject(browser, link)
-    page_object.test_message_disappeared_after_adding_product_to_basket()'''
+    page_object.test_message_disappeared_after_adding_product_to_basket()
+
 
 def test_login_page_checking(browser):
     link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
     page_object = PageObject(browser, link)
     page_object.test_guest_should_see_login_link_on_product_page()
 
+
 def test_go_to_login_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
     page_object = PageObject(browser, link)
     page_object.test_guest_can_go_to_login_page_from_product_page()
 
+@pytest.fixture(scope='function')
+def registration_users(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    page = LoginPage(browser, link)
+    email = page.generation_email()
+    password = page.generation_password()
+    page.register_new_user(email, password)
 
+
+def test_user_can_add_product_to_basket(browser, registration_users):
+    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+    page_object = PageObject(browser, link)
+    page_object.open()
+    page_object.expected_product_name_and_price()
+    page_object.add_to_basket()
+    page_object.check_correct_product_name()
+    page_object.check_correct_product_price()
+
+def test_user_cant_see_success_message(browser, registration_users):
+    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207'
+    pages_checking = PageObject(browser, link)
+    pages_checking.open()
+    pages_checking.add_to_basket()
+    assert pages_checking.is_element_present(*AddToBasketTest.REAL_NAME_PRODUCT), 'Element is not present'
